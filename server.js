2d0af35dd7d88edf12d3e343ababab3354929461
Tyler Pierson <tylerpierson92@gmail.com>
2024-01-18 19:42:55 -0600

    Created blank controller files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
 mongoose.connect(process.env.MONGO_URI)
-mongoose.connection.once('open', () => console.log('Mongo is showing love'))
+mongoose.connection.once('open', () => console.log('Mongo is ACTIVE'))
 
 app.list(PORT, () => {
     console.log(`Port is running @ ${PORT}`)
