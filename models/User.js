53e73d545e1c8de2eb745f13fdeb51a477b6ffe8
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-01-22 11:17:46 -0600

    Changed my default role to be admin instead of teacher

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         enum: ['admin', 'teacher', 'student'],
-        default: 'teacher'
+        default: 'admin'
     },
     campus: {
         type: String,
